b915925702010e33a6fcd507a665f725e24a88bb
Nazar Vashchyshyn <nazar.ua87@gmail.com>
2021-12-11 14:29:38 +0200

    uploaded structure

---
+++ src/main/java/dao/AuthorDAO.java
@@ -0,0 +1,22 @@
+package dao;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class AuthorDAO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String author_name;
+
+    private
+
+    public Integer getId() {
+        return id;
+    }
+}
