761fd3ceead8a279a4418c350dd751fe45dd1915
Oksana <marjash@i.ua>
2021-12-21 23:37:59 +0200

    modified files

--- src/main/java/com/company/jcc/repository/BookRepository.java
+++ src/main/java/com/company/jcc/repository/BookRepository.java
@@ -1,9 +1,61 @@
 package com.company.jcc.repository;
 
 import com.company.jcc.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.company.jcc.service.BookService;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Repository
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public class BookRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public void create(Book book) {
+        entityManager.persist(book);
+    }
+
+    @Transactional
+    public List<Book> getAll(){
+        TypedQuery<Book> query = entityManager.createQuery("from Book", Book.class);
+        List<Book> list = query.getResultList();
+        return list;
+    }
+
+//    @Override
+//    public Book readById(int id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Book update(Book book) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void delete(int id) {
+//
+//    }
+//
+//    @Override
+//    public List<Book> getAll() {
+//        return null;
+//    }
+
+    public BookRepository() {
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 }
--- src/main/java/com/company/jcc/service/impl/BookServiceImpl.java
+++ src/main/java/com/company/jcc/service/impl/BookServiceImpl.java
@@ -4,46 +4,60 @@ import com.company.jcc.model.Book;
 import com.company.jcc.model.Rent;
 import com.company.jcc.repository.BookRepository;
 import com.company.jcc.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Service
 public class BookServiceImpl implements BookService {
+
+    @Autowired
     private final BookRepository bookRepository;
 
     public BookServiceImpl(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
-    @Override
-    public Book create(Book book) {
-        return bookRepository.save(book);
-    }
-
-    @Override
-    public Book readById(int id) {
-        return bookRepository.findById(id).orElseThrow(
-                () -> new EntityNotFoundException("Book with id " + id + " not found"));
-    }
-
-    @Override
-    public Book update(Book book) {
-//        if (role != null) {
-        readById(book.getId());
-        return bookRepository.save(book);
-//        }
-    }
-
-    @Override
-    public void delete(int id) {
-        Book book = readById(id);
-        bookRepository.delete(book);
-    }
+//    public void create(String bookTitle, int amountLeft, int amountGave, int rating){
+//        bookRepository.create(new Book(bookTitle, amountLeft, amountGave, rating));
+//    }
 
     @Override
     public List<Book> getAll() {
-        return bookRepository.findAll();
+        return bookRepository.getAll();
     }
+
+    @Override
+    public void create(Book book) {
+        bookRepository.create(book);
+    }
+
+
+//    @Override
+//    public Book readById(int id) {
+//        return bookRepository.findById(id).orElseThrow(
+//                () -> new EntityNotFoundException("Book with id " + id + " not found"));
+//    }
+//
+//    @Override
+//    public Book update(Book book) {
+////        if (role != null) {
+//        readById(book.getId());
+//        return bookRepository.save(book);
+////        }
+//    }
+//
+//    @Override
+//    public void delete(int id) {
+//        Book book = readById(id);
+//        bookRepository.delete(book);
+//    }
+//
+//    @Override
+//    public List<Book> getAll() {
+//        return bookRepository.findAll();
+//    }
 }
