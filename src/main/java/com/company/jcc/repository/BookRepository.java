655c45e023872f33c26e2eb14d3392a926efa4e8
Nazar Vashchyshyn <nazar.ua87@gmail.com>
2021-12-22 18:58:52 +0200

    .

--- src/main/java/com/company/jcc/repository/BookRepository.java
+++ src/main/java/com/company/jcc/repository/BookRepository.java
@@ -1,9 +1,36 @@
 package com.company.jcc.repository;
 
 import com.company.jcc.model.Book;
+import com.company.jcc.service.BookService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface BookRepository extends JpaRepository<Book, Integer> {
+public class BookRepository implements BookService {
+    @Override
+    public Book create(Book book) {
+        return null;
+    }
+
+    @Override
+    public Book readById(int id) {
+        return null;
+    }
+
+    @Override
+    public Book update(Book book) {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return null;
+    }
 }
